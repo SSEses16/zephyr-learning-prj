46cb57c13bd68236b7e9a0cb0d3f3d529c926e6b
Spiridonov Sergey <s-spiridonov.4@mail.ru>
2026-01-25 18:44:48 +0300

    56325432523

--- src/main.c
+++ src/main.c
@@ -7,12 +7,14 @@
 
 /* The devicetree node identifier for the "led0" alias. */
 #define LED0_NODE DT_ALIAS(led0)
+#define LED1_NODE DT_ALIAS(led1)
 
 /*
  * A build error on this line means your board is unsupported.
  * See the sample documentation for information on how to fix this.
  */
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+static const struct gpio_dt_spec led_1 = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
 
 int main(void)
 {
@@ -28,8 +30,18 @@ int main(void)
 		return 0;
 	}
 
+	ret = gpio_pin_configure_dt(&led_1, GPIO_OUTPUT_INACTIVE);
+	if (ret < 0) {
+		return 0;
+	}
+
 	while (1) {
 		ret = gpio_pin_toggle_dt(&led);
+		if (ret < 0) {
+			return 0;
+		}
+
+		ret = gpio_pin_toggle_dt(&led_1);
 		if (ret < 0) {
 			return 0;
 		}
